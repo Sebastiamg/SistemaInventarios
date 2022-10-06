f1a62f7309a5d5c0365e7945de2dcd57a1dc82ac
Sebastiamg <sebasortiz112004@gmail.com>
2022-10-06 11:13:32 -0500

    presentacionFinal

--- backend/xampp/htdocs/rest-api/api/xml/create_xml.php
+++ backend/xampp/htdocs/rest-api/api/xml/create_xml.php
@@ -12,25 +12,74 @@ header("HTTP/1.1 200 OK");
 die();
 }
 
-//$data = json_decode(file_get_contents("php://input")); 
-
-
-
 $liquidacionCompraJ = file_get_contents("php://input");
 $decodedJson = json_decode($liquidacionCompraJ, true );
 
-print_r($decodedJson);
+//print_r($decodedJson);
 
-//echo $decodedJson ["liquidacionCompra"]["id"];
+foreach ($decodedJson['detalles'] as $detalle) {
+    /* print_r ($detalle); */
+    //validacion detalles
+    $cantidad = $detalle['detalle']['cantidad'];
+    /* print_r($cantidad ); */
+    $precioUnitario = $detalle['detalle']['precioUnitario'];
+    /*  print_r( $precioUnitario); */
+    $descuento = $detalle['detalle']['descuento'];
+    /* print_r( $descuento); */
+    $precioTotalSinImpuesto = $detalle['detalle']['precioTotalSinImpuesto']; 
+    //formulas
+    /* print_r( $precioTotalSinImpuesto) */ 
+    $validacionTotalSinImpuesto =($cantidad * $precioUnitario * (100 - floatval($descuento))) / 100;
+    /*  print_r($validacionTotalSinImpuesto); */
+    if ($validacionTotalSinImpuesto == $precioTotalSinImpuesto) {
+    } else {
+        print_r("Validacion Incorrecta Por favor Corrija \n\n\n");
+    }
+};
+    foreach ($detalle['impuestos'] as $impuesto) {
+    //validacion impuestos
+    $tarifa = $impuesto['impuestos']['tarifa'];
+    $baseImponible = $impuesto['impuestos']['baseImponible'];
+    $valor = $impuesto['impuestos']['valor'];
+    //formulas
+    $valorFinal = ($baseImponible * $tarifa) / 100;
+    $valorFinal = round($valorFinal, 2);
+    if ($valorFinal == $valor) {
+    } else {
+        echo 'Valores finales Incorrectos' . $valor . ' ' . $valorFinal;
+    }
+    if ($baseImponible == $validacionTotalSinImpuesto) {
+    } else {
+        echo "Validacion 2 Incorrecta \"CORRIJA\" \n";
+    }
+}
+    //importe Total
+    $info = $decodedJson ['infoLiquidacionCompra'] -> importeTotal;
+    echo $info;
+    $total1 =  $decodedJson ['infoLiquidacionCompra'] -> totalConImpuestos;
 
-//echo $decodedJson ["liquidacionCompra"]["infoTributaria"]["ambiente"];
+    foreach ($total1  as  $value) {
+        $total2 = $value -> totalImpuesto -> valor;
+        /* echo $total2 ; */
+    }
 
+    $total3 = $decodedJson ['infoLiquidacionCompra'] -> totalSinImpuestos;
+    /* echo $total3; */
+
+    $total4 = $total2 + $total3;
+    echo $total4;
+
+    if($info == $total4 ){
+    }else{
+        echo "Importe total Incorrecto";
+    }
 
 $liquidacionCompra = simplexml_load_file('LiquidacionCompra_V1.1.0.xml');
-//Se aplica el de leer xml para traer los primero datos que estan en liquidacion compa: xml, xsl, etc
+
 $liquidacionCompra -> infoTributaria -> ambiente = $decodedJson ["infoTributaria"]["ambiente"];
 $liquidacionCompra -> infoTributaria -> tipoEmision =$decodedJson ["infoTributaria"]["tipoEmision"];
 $liquidacionCompra -> infoTributaria -> razonSocial = $decodedJson ["infoTributaria"]["razonSocial"];
+$liquidacionCompra -> infoTributaria -> nombreComercial = $decodedJson ["infoTributaria"]["nombreComercial"];
 $liquidacionCompra -> infoTributaria -> ruc = $decodedJson ["infoTributaria"]["ruc"];
 $liquidacionCompra -> infoTributaria -> claveAcceso = $decodedJson ["infoTributaria"]["claveAcceso"];
 $liquidacionCompra -> infoTributaria -> codDoc = $decodedJson ["infoTributaria"]["codDoc"];
@@ -38,14 +87,15 @@ $liquidacionCompra -> infoTributaria -> estab = $decodedJson ["infoTributaria"][
 $liquidacionCompra -> infoTributaria -> ptoEmi = $decodedJson ["infoTributaria"]["ptoEmi"];
 $liquidacionCompra -> infoTributaria -> secuencial = $decodedJson ["infoTributaria"]["secuencial"];
 $liquidacionCompra -> infoTributaria -> dirMatriz = $decodedJson ["infoTributaria"]["dirMatriz"];
-$liquidacionCompra -> infoTributaria -> agenteRetencion = $decodedJson ["infoTributaria"]["agenteRetencion"];
-$liquidacionCompra -> infoTributaria -> contribuyenteRimpe = $decodedJson ["infoTributaria"]["contribuyenteRimpe"];
+$liquidacionCompra -> infoTributaria -> agenteRetencion = "No";
+$liquidacionCompra -> infoTributaria -> contribuyenteRimpe =  "No";
 
 $liquidacionCompra -> infoLiquidacionCompra -> fechaEmision = $decodedJson ["infoLiquidacionCompra"]["fechaEmision"];
 $liquidacionCompra -> infoLiquidacionCompra -> dirEstablecimiento = $decodedJson ["infoLiquidacionCompra"]["dirEstablecimiento"];
 $liquidacionCompra -> infoLiquidacionCompra -> contribuyenteEspecial = $decodedJson ["infoLiquidacionCompra"]["contribuyenteEspecial"];
 $liquidacionCompra -> infoLiquidacionCompra -> obligadoContabilidad = $decodedJson ["infoLiquidacionCompra"]["obligadoContabilidad"];
 $liquidacionCompra -> infoLiquidacionCompra -> tipoIdentificacionProveedor = $decodedJson ["infoLiquidacionCompra"]["tipoIdentificacionProveedor"];
+$liquidacionCompra -> infoLiquidacionCompra -> identificacionProveedor = $decodedJson ["infoLiquidacionCompra"]["identificacionProveedor"];
 $liquidacionCompra -> infoLiquidacionCompra -> razonSocialProveedor = $decodedJson ["infoLiquidacionCompra"]["razonSocialProveedor"];
 $liquidacionCompra -> infoLiquidacionCompra -> direccionProveedor = $decodedJson ["infoLiquidacionCompra"]["direccionProveedor"];
 $liquidacionCompra -> infoLiquidacionCompra -> totalSinImpuestos = $decodedJson ["infoLiquidacionCompra"]["totalSinImpuestos"];
@@ -55,38 +105,38 @@ $liquidacionCompra -> infoLiquidacionCompra -> totalComprobantesReembolso = $dec
 $liquidacionCompra -> infoLiquidacionCompra -> totalBaseImponibleReembolso = $decodedJson ["infoLiquidacionCompra"]["totalBaseImponibleReembolso"];
 $liquidacionCompra -> infoLiquidacionCompra -> totalImpuestoReembolso = $decodedJson ["infoLiquidacionCompra"]["totalImpuestoReembolso"];
 
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto -> codigo = $decodedJson ["totalConImpuestos"]["codigo"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->codigoPorcentaje = $decodedJson ["totalConImpuestos"]["codigoPorcentaje"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->descuentoAdicional = $decodedJson ["totalConImpuestos"]["descuentoAdicional"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->baseImponible = $decodedJson ["totalConImpuestos"]["baseImponible"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->tarifa = $decodedJson ["totalConImpuestos"]["tarifa"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->valor = $decodedJson ["totalConImpuestos"]["valor"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->importeTotal = $decodedJson ["totalConImpuestos"]["importeTotal"];
-$liquidacionCompra -> totalConImpuestos -> totalImpuesto ->moneda = $decodedJson ["totalConImpuestos"]["moneda"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto -> codigo = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["codigo"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto ->codigoPorcentaje = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["codigoPorcentaje"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto ->descuentoAdicional = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["descuentoAdicional"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto ->baseImponible = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["baseImponible"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto ->tarifa = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["tarifa"];
+$liquidacionCompra -> infoLiquidacionCompra -> totalConImpuestos -> totalImpuesto ->valor = $decodedJson ["infoLiquidacionCompra"]["totalConImpuestos"][0]["totalImpuesto"]["valor"];
 
-$liquidacionCompra -> pagos -> pago -> formaPago = $decodedJson ["pagos"]["pago"]["formaPago"];
-$liquidacionCompra -> pagos -> pago ->total = $decodedJson ["pagos"]["pago"]["total"];
-$liquidacionCompra -> pagos -> pago ->plazo = $decodedJson ["pagos"]["pago"]["plazo"];
-$liquidacionCompra -> pagos -> pago ->unidadTiempo = $decodedJson ["pagos"]["pago"]["unidadTiempo"];
+$liquidacionCompra -> infoLiquidacionCompra -> importeTotal = $decodedJson ["infoLiquidacionCompra"]["importeTotal"];
+$liquidacionCompra -> infoLiquidacionCompra -> moneda = $decodedJson ["infoLiquidacionCompra"]["moneda"];
 
-$liquidacionCompra -> detalles -> detalle -> codigoPrincipal = $decodedJson ["detalles"]["detalle"]["codigoPrincipal"];
-$liquidacionCompra -> detalles -> detalle ->codigoAuxiliar = $decodedJson ["detalles"]["detalle"]["codigoAuxiliar"];
-$liquidacionCompra -> detalles -> detalle ->descripcion = $decodedJson ["detalles"]["detalle"]["descripcion"];
-$liquidacionCompra -> detalles -> detalle ->unidadMedida = $decodedJson ["detalles"]["detalle"]["unidadMedida"];
-$liquidacionCompra -> detalles -> detalle ->cantidad = $decodedJson ["detalles"]["detalle"]["cantidad"];
-$liquidacionCompra -> detalles -> detalle ->precioUnitario = $decodedJson ["detalles"]["detalle"]["precioUnitario"];
-$liquidacionCompra -> detalles -> detalle ->precioSinSubsidio = $decodedJson ["detalles"]["detalle"]["precioSinSubsidio"];
-$liquidacionCompra -> detalles -> detalle ->descuento = $decodedJson ["detalles"]["detalle"]["descuento"];
-$liquidacionCompra -> detalles -> detalle ->precioTotalSinImpuesto = $decodedJson ["detalles"]["detalle"]["precioTotalSinImpuesto"];
+foreach($decodedJson ["infoLiquidacionCompra"]["pagos"] as $pagosAux){
+    $pagos = $liquidacionCompra -> infoLiquidacionCompra -> pagos ->addChild('pago');
+    $pagos->addChild('formaPago', $pagosAux["pago"]["formaPago"]);
+    $pagos->addChild('total', $pagosAux["pago"]["total"]);
+	$pagos->addChild('plazo', $pagosAux["pago"]["plazo"]);
+	$pagos->addChild('unidadTiempo', $pagosAux["pago"]["unidadTiempo"]);
+}
 
-$liquidacionCompra -> detallesAdicionales -> detAdicional = $decodedJson ["detallesAdicionales"]["detAdicional"];
+foreach($decodedJson ["detalles"] as $detallesAux){
 
-$liquidacionCompra -> impuestos -> impuesto ->codigo = $decodedJson ["impuestos"]["impuesto"]["codigo"];
-$liquidacionCompra -> impuestos -> impuesto ->codigoPorcentaje = $decodedJson ["impuestos"]["impuesto"]["codigoPorcentaje"];
-$liquidacionCompra -> impuestos -> impuesto ->tarifa = $decodedJson ["impuestos"]["impuesto"]["tarifa"];
-$liquidacionCompra -> impuestos -> impuesto ->baseImponible = $decodedJson ["impuestos"]["impuesto"]["baseImponible"];
-$liquidacionCompra -> impuestos -> impuesto ->valor = $decodedJson ["impuestos"]["impuesto"]["valor"];
-
+    $detalles = $liquidacionCompra -> detalles -> addChild('detalle');
+    $detalles->addChild('codigoPrincipal', $detallesAux["detalle"]["codigoPrincipal"]);
+    $detalles->addChild('codigoAuxiliar', $detallesAux["detalle"]["codigoAuxiliar"]);
+	$detalles->addChild('descripcion', $detallesAux["detalle"]["descripcion"]);
+	$detalles->addChild('unidadMedida', $detallesAux["detalle"]["unidadMedida"]);
+    $detalles->addChild('cantidad', $detallesAux["detalle"]["cantidad"]);
+    $detalles->addChild('precioUnitario', $detallesAux["detalle"]["precioUnitario"]);
+	$detalles->addChild('precioSinSubsidio', $detallesAux["detalle"]["precioSinSubsidio"]);
+	$detalles->addChild('descuento', $detallesAux["detalle"]["descuento"]);
+    $detalles->addChild('precioTotalSinImpuesto', $detallesAux["detalle"]["precioTotalSinImpuesto"]);
+	$detalles->addChild('detAdicional', $detallesAux["detalle"]["detAdicional"]);
+}
 
 $liquidacionCompra -> reembolsos -> reembolsoDetalle -> tipoIdentificacionProveedorReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["tipoIdentificacionProveedorReembolso"];
 $liquidacionCompra -> reembolsos -> reembolsoDetalle ->identificacionProveedorReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["identificacionProveedorReembolso"];
@@ -96,20 +146,22 @@ $liquidacionCompra -> reembolsos -> reembolsoDetalle ->codDocReembolso = $decode
 $liquidacionCompra -> reembolsos -> reembolsoDetalle ->estabDocReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["estabDocReembolso"];
 $liquidacionCompra -> reembolsos -> reembolsoDetalle ->ptoEmiDocReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["ptoEmiDocReembolso"];
 $liquidacionCompra -> reembolsos -> reembolsoDetalle ->secuencialDocReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["secuencialDocReembolso"];
-$liquidacionCompra -> reembolsos -> reembolsoDetalle ->fechaEmisionDocReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["fechaEmisionDocReembolso"];
-$liquidacionCompra -> reembolsos -> reembolsoDetalle ->numeroautorizacionDocReemb = $decodedJson ["reembolsos"]["reembolsoDetalle"]["numeroautorizacionDocReemb"];
 
-$liquidacionCompra -> detalleImpuestos -> detalleImpuesto -> codigo = $decodedJson ["detalleImpuestos"]["detalleImpuesto"]["codigo"];
-$liquidacionCompra -> detalleImpuestos -> detalleImpuesto ->codigoPorcentaje = $decodedJson ["detalleImpuestos"]["detalleImpuesto"]["codigoPorcentaje"];
-$liquidacionCompra -> detalleImpuestos -> detalleImpuesto ->tarifa = $decodedJson ["detalleImpuestos"]["detalleImpuesto"]["tarifa"];
-$liquidacionCompra -> detalleImpuestos -> detalleImpuesto ->baseImponibleReembolso = $decodedJson ["detalleImpuestos"]["detalleImpuesto"]["baseImponibleReembolso"];
-$liquidacionCompra -> detalleImpuestos -> detalleImpuesto ->impuestoReembolso = $decodedJson ["detalleImpuestos"]["detalleImpuesto"]["impuestoReembolso"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle ->fechaEmisionDocReembolso = $decodedJson ["reembolsos"]["reembolsoDetalle"]["detalleImpuestos"]["detalleImpuesto"]["fechaEmisionDocReembolso"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle ->numeroautorizacionDocReemb = $decodedJson ["reembolsos"]["reembolsoDetalle"]["detalleImpuestos"]["detalleImpuesto"]["numeroautorizacionDocReemb"];
 
-$liquidacionCompra -> tipoNegociable -> correo = $decodedJson ["tipoNegociable"]["correo"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle -> detalleImpuestos -> detalleImpuesto -> codigo = $decodedJson ["reembolsos"]["reembolsoDetalle"]["detalleImpuestos"]["detalleImpuesto"]["codigo"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle -> detalleImpuestos -> detalleImpuesto -> codigoPorcentaje = $decodedJson ["reembolsos"]["reembolsoDetalle"]["detalleImpuestos"]["detalleImpuesto"]["codigoPorcentaje"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle -> detalleImpuestos -> detalleImpuesto -> tarifa = $decodedJson ["reembolsos"]["reembolsoDetalle"]["detalleImpuestos"]["detalleImpuesto"]["tarifa"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle -> detalleImpuestos -> detalleImpuesto -> baseImponibleReembolso = $decodedJson ["maquinaFiscal"]["baseImponibleReembolso"];
+$liquidacionCompra -> reembolsos -> reembolsoDetalle -> detalleImpuestos -> detalleImpuesto -> impuestoReembolso = $decodedJson ["maquinaFiscal"]["impuestoReembolso"];
+
+$liquidacionCompra -> tipoNegociable = $decodedJson ["tipoNegociable"];
 
 $liquidacionCompra -> maquinaFiscal ->marca = $decodedJson ["maquinaFiscal"]["marca"];
 $liquidacionCompra -> maquinaFiscal ->modelo = $decodedJson ["maquinaFiscal"]["modelo"];
 $liquidacionCompra -> maquinaFiscal ->serie = $decodedJson ["maquinaFiscal"]["serie"];
+
 $ruc=$decodedJson ["infoTributaria"]["ruc"];
 print_r($liquidacionCompra);
 
